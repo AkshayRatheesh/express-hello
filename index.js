0929361af0bdba7077f1e96757f485d58f4c2f16
AKSHAY RATHEESH <92791421+AkshayRatheesh@users.noreply.github.com>
2022-07-27 13:57:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('<h1 style="margin:30px">Express</h1><p style="margin:30px">Welcome to Express</p>')
 })
 
 app.listen(3000, function () {
